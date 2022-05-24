0735e11fbafa79d32c8fd88468826aeb9ed0fc11
unknown <oficialfcsj@gmail.com>
2022-05-23 22:33:54 -0400

    codigo da aula 19-05

--- src/pacote/AnalisadorSemantico.java
+++ src/pacote/AnalisadorSemantico.java
@@ -5,28 +5,24 @@ import java.util.List;
 
 public class AnalisadorSemantico {
 	
-	
-	
 	public List <String> tabelaSimbolos;
 	public List <String> dicionario;
 	
-	
 	public AnalisadorSemantico() {
 		tabelaSimbolos = new ArrayList<>();
-		
 		
 	}
 	
 	public void analisar(List <String> exemplo, List<String> dicionario) {
 		// this. to pegando da tabela privada e movendo p tabela publica
-		int erros = 0;
 		this.dicionario = dicionario;
+		int erros = 0;
 		int nl = 1; 
 		for (String linha: exemplo) {
 			erros += validarLinha( linha.trim(), nl);
 			nl++;
 		}
-
+		System.out.println("Fim da Analise SemÃ¢ntica: erros = " + erros);
 	}
 	// token = uma palavra
 	// tokens = todas as palavras
@@ -37,43 +33,49 @@ public class AnalisadorSemantico {
 // System.out.println("Tabela de simbolos" + tabelaSimbolos);
 		for(String token: tokens) {
 			
-			if(tokens[0].equals("escreva") && tokens[1].startsWith("\"") ) {
-				System.out.println("oq é isso " + token);
+			if(!(tokens[0].equals("escreva") && tokens[1].startsWith("\""))) {
+
+				if( numeroLinha > 0 &&  ehVariavel(token)  ) {
+					System.out.println("Ã‰ Variavel: " + token);
+					
 				
+	
+				}
+				if(tokens[0].equals("int") || 
+				   tokens[0].equals("char") ||
+				   tokens[0].equals("bool") || 
+				   tokens[0].equals("float"))
+					{
+					tabelaSimbolos.add(token);
+				}
+				
+				else{
+					if(tabelaSimbolos.contains(token)) {
+						// System.out.println("Variavel OK: " + token);
+						return 0;
+					}else{
+						System.out.println(" Linha: " + nl + " Variavel nÃ£o declarada: " + token);
+						return 1;
+					}
+				}		
+				numeroLinha++;
 			}
-			if( numeroLinha > 0 &&  ehVariavel(token)  ) {
-				System.out.println("Variavel: " + token);
-				
-			
+
+
 
 			}
-			if(tokens[0].equals("int") || 
-			   tokens[0].equals("char") ||
-			   tokens[0].equals("bool") || 
-			   tokens[0].equals("float")) {
-				tabelaSimbolos.add(token);
-			}
-			
-			else{
-				if(tabelaSimbolos.contains(token)) {
-					System.out.println("Variavel OK: " + token);
-					return 0;
-				}else{
-					System.out.println("Variavel não declarada: " + token + " Linha: " + nl);
-					return 1;
-				}
-			}		
-			
-		}
+		
 		return 0;
 	}
 
 	public boolean ehVariavel(String token) {
-		
-		if(dicionario.contains(token)) {
+		if(token.isEmpty()){
 			return false;
 		}
-		else{
+
+		if(dicionario.contains(token)) {
+			return false;
+		} else {
 			if(token.matches("^[0-9]{1,10}$")) {
 				return false;
 			}
